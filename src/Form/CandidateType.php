f5b2ba5654e39bb0ce4fffbd347295f9a2960500
ahmedobeidi <alobeidi.dev@gmail.com>
2025-02-11 09:54:01 +0100

    CONVERT: candidate html form to twig

--- src/Form/CandidateType.php
+++ src/Form/CandidateType.php
@@ -6,8 +6,10 @@ use App\Entity\Candidate;
 use App\Entity\Gender;
 use App\Entity\User;
 use DateTimeImmutable;
+use Doctrine\DBAL\Types\DateImmutableType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -73,6 +75,33 @@ class CandidateType extends AbstractType
                 ],
                 'label' => 'Country'
             ])
+            ->add('nationality', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'id' => 'nationality'
+                ],
+                'label' => 'Nationality'
+            ])
+            ->add('birthPlace', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'id' => 'birth_place'
+                ],
+                'label' => 'Birthplace'
+            ])
+            ->add('birthDate', BirthdayType::class, [
+                'required' => false,
+                'label' => 'Birthdate',
+                // 'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'datepicker',
+                    'id' => 'birth_date',
+                ],
+                'label_attr' => [
+                    'class' => 'active',
+                ],
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('profilePictureFile', FileType::class,[
                 'mapped' => false,
                 'required' => false,
@@ -91,6 +120,50 @@ class CandidateType extends AbstractType
                     'accept' => '.jpg,.jpeg,.png,.gif',
                     'id' => 'photo',
                 ]
+            ])
+            ->add('passportFile', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'accept' => '.pdf,.jpg,.doc,.docx,.png,.jpeg',
+                    'size' => 200000000,
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '200M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                            'application/pdf',
+                            'application/msword',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('cvFile', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'accept' => '.pdf,.jpg,.doc,.docx,.png,.jpeg',
+                    'size' => 200000000,
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '200M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                            'application/pdf',
+                            'application/msword',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
             ])
             ->addEventListener(FormEvents::POST_SUBMIT, $this->setUpdatedAt(...))
         ;
